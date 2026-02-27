9230e33d89c390a08fdd624fd514116b1948ccf8
AleTane04 <gaetanoalessioforino@gmail.com>
2026-02-27 10:07:48 +0100

    Persona

---
+++ src/main/java/org/aletane04/people/Persona.java
@@ -0,0 +1,112 @@
+package org.aletane04.people;
+import java.io.Serializable;
+import java.time.*;
+import java.util.*;
+
+public class Persona implements Comparable<Persona>, Serializable
+{
+    private static int count = 0;
+    private int number;
+    private String nome;
+    private String cognome;
+    private LocalDate dataNascita;
+    private String codiceFiscale;
+
+    public Persona(String nome, String cognome, LocalDate dataNascita, String codiceFiscale)
+    {
+        this.nome=nome;
+        this.cognome=cognome;
+        this.dataNascita=dataNascita;
+        this.codiceFiscale=codiceFiscale;
+        this.number = ++count;
+    }
+
+    /* Metodi getter */
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public String getCognome()
+    {
+        return cognome;
+    }
+
+    public LocalDate getDataNascita()
+    {
+        return dataNascita;
+    }
+
+    public String getCodiceFiscale()
+    {
+        return codiceFiscale;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    /* Metodi setter */
+
+    public void setNome(String nome)
+    {
+        this.nome=nome;
+    }
+
+    public void setCognome(String cognome)
+    {
+        this.cognome=cognome;
+    }
+
+    public void setDataNascita(LocalDate dataNascita)
+    {
+        this.dataNascita=dataNascita;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale)
+    {
+        this.codiceFiscale=codiceFiscale;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof Persona))
+            return false;
+        /* Downcast per poter operare su p */
+        Persona p = (Persona) obj;
+
+        return this.codiceFiscale.equals(p.codiceFiscale);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        /*return Objects.hash(this.codiceFiscale); */
+        int code = codiceFiscale == null ? 0 : codiceFiscale.hashCode();
+        return code;
+    }
+
+    @Override
+    public int compareTo(Persona p)
+    {
+        if(this.nome.equals(p.nome))
+        {
+            if(this.cognome.equals(p.cognome))
+            {
+                if(this.dataNascita.equals(p.dataNascita))
+                {
+                    return this.codiceFiscale.compareTo(p.codiceFiscale);
+                }
+                return this.dataNascita.compareTo(p.dataNascita);
+            }
+            return this.cognome.compareTo(p.cognome);
+        }
+        return this.nome.compareTo(p.nome);
+    }
+
+
+}
